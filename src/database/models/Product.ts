73572a2c3e10da7bf4b7f7e9be71876cfb547fc6
leandroalves <leandro.alves@lighthouseit.com.br>
2022-08-31 01:17:31 -0300

    finish listing

--- src/database/models/Product.ts
+++ src/database/models/Product.ts
@@ -19,6 +19,35 @@ const Product = connetion.sequelize.define('product',{
     ListPrice_Product:{
         type: connetion.Sequelize.DOUBLE
     },
-})
+});
 
-export default Product;
+export const getItems = async (res: any) => {
+    try {
+        const allProducts = await Product.findAll();
+
+        return res.status(200).json(allProducts);
+    } catch{
+        return res.status(400).json({menssage: "serve erro"});
+    }
+}
+
+export const getItem = async (res: any, id: number) => {
+    try {
+        const product = await Product.findOne({ where: { id } });
+        if (product === null) {
+            return res.status(404).json({menssage: "not found!"});
+          } else {
+            return res.status(200).json(product);
+          }
+    } catch{
+        return res.status(400).json({menssage: "serve erro"});
+    }
+}
+// Product.create({
+//     Title_Product: "Notebook VAIO® FE15 Intel® Core™ i7 Windows 11 Home 8GB 512GB SSD Full HD - Prata",
+//     Thumbnail_Product: "https://images-americanas.b2w.io/produtos/5267258975/imagens/notebook-vaio-fe15-intel-core-i5-linux-8gb-256gb-ssd-hd-prata/5267259038_1_large.jpg",
+//     Stock_Product: 5,
+//     Avaliation_Product:5,
+//     SalePrice_Product: 500.00,
+//     ListPrice_Product: 600.00
+// })
--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -1,17 +1,15 @@
 import express from 'express'; 
-import Product from '../database/models/Product';
+import {getItems, getItem} from '../database/models/Product';
 
-const productRouter = express.Router();
+const productRouter  = express.Router();
 
 productRouter.get('/products', (req, res) => {
-    res.send('Lê todos os product');
-    Product.findAll().then(function(teste: any){
-        console.log('todos', teste)
-    });
+    getItems(res);
 });
 
 productRouter.get('/product/:id', (req, res) => {
     const id: number = +req.params.id;
-    res.send(`Lê o product ${id}`);
+    getItem(res, id)
 })
+
 export default productRouter;
