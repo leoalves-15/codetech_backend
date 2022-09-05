55d8073a85aecc39f45e9f4b7d665c2a841714bb
leandroalves <leandro.alves@lighthouseit.com.br>
2022-09-05 11:28:44 -0300

    selected more on item

--- src/database/models/Product.ts
+++ src/database/models/Product.ts
@@ -1,45 +1,70 @@
-const connetion = require('../connection');
+const connetion = require("../connection");
 
-const Product = connetion.sequelize.define('product',{
-    Title_Product:{
-        type: connetion.Sequelize.STRING
-    }, 
-    Thumbnail_Product:{
-        type: connetion.Sequelize.STRING
-    }, 
-    Stock_Product:{
-        type: connetion.Sequelize.DOUBLE
-    },
-    Avaliation_Product:{
-        type: connetion.Sequelize.DOUBLE
-    },
-    SalePrice_Product:{
-        type: connetion.Sequelize.DOUBLE
-    },
-    ListPrice_Product:{
-        type: connetion.Sequelize.DOUBLE
-    },
+const Product = connetion.sequelize.define("product", {
+  Title_Product: {
+    type: connetion.Sequelize.STRING,
+  },
+  Thumbnail_Product: {
+    type: connetion.Sequelize.STRING,
+  },
+  Stock_Product: {
+    type: connetion.Sequelize.DOUBLE,
+  },
+  Avaliation_Product: {
+    type: connetion.Sequelize.DOUBLE,
+  },
+  SalePrice_Product: {
+    type: connetion.Sequelize.DOUBLE,
+  },
+  ListPrice_Product: {
+    type: connetion.Sequelize.DOUBLE,
+  },
 });
 
 export const getItems = async (res: any) => {
-    try {
-        const allProducts = await Product.findAll();
+  try {
+    const allProducts = await Product.findAll();
 
-        return res.status(200).json(allProducts);
-    } catch{
-        return res.status(400).json({menssage: "serve erro"});
+    return res.status(200).json(allProducts);
+  } catch {
+    return res.status(400).json({ menssage: "serve erro" });
+  }
+};
+
+export const getItemRequest = async (res: any, id: number) => {
+  try {
+    const product = await Product.findOne({ where: { id } });
+    if (product === null) {
+      return "not found!";
+    } else {
+      return product;
     }
-}
+  } catch {
+    return "serve erro";
+  }
+};
 
 export const getItem = async (res: any, id: number) => {
-    try {
-        const product = await Product.findOne({ where: { id } });
-        if (product === null) {
-            return res.status(404).json({menssage: "not found!"});
-          } else {
-            return res.status(200).json(product);
-          }
-    } catch{
-        return res.status(400).json({menssage: "serve erro"});
+  try {
+    const product = await getItemRequest(res, id);
+    if (product === "not found!") {
+      return res.status(404).json({ menssage: "not found!" });
+    } else {
+      return res.status(200).json(product);
     }
-}
+  } catch {
+    return res.status(400).json({ menssage: "serve erro" });
+  }
+};
+
+export const getItemsSelected = async (res: any, id: string[]) => {
+  try {
+    let products = [];
+    for (let i in id) {
+      let productA = await getItemRequest(res, parseInt(id[i]));
+      products.push(productA);
+    }
+  } catch {
+    return res.status(400).json({ menssage: "serve erro" });
+  }
+};
--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -1,15 +1,24 @@
-import express from 'express'; 
-import {getItems, getItem} from '../database/models/Product';
+import express from "express";
+import {
+  getItems,
+  getItem,
+  getItemsSelected,
+} from "../database/models/Product";
 
-const productRouter  = express.Router();
+const productRouter = express.Router();
 
-productRouter.get('/products', (req, res) => {
-    getItems(res);
+productRouter.get("/products", (req, res) => {
+  getItems(res);
 });
 
-productRouter.get('/product/:id', (req, res) => {
-    const id: number = +req.params.id;
-    getItem(res, id)
-})
+productRouter.get("/products/ids/:ids", (req, res) => {
+  var arrayIDS = req.params.ids.split("_");
+  getItemsSelected(res, arrayIDS);
+});
+
+productRouter.get("/product/:id", (req, res) => {
+  const id: number = +req.params.id;
+  getItem(res, id);
+});
 
 export default productRouter;
