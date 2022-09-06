51da0c6d9fc2aa8602e278d7f71019be64c18e0c
leandroalves <leandro.alves@lighthouseit.com.br>
2022-09-05 21:50:36 -0300

    configure var

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,18 @@
-const Sequelize = require('sequelize');
-import {bdDatas} from "../../env";
+const Sequelize = require("sequelize");
+
+const bdDatas = {
+  bd: process.env.BD,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+};
 
 const sequelize = new Sequelize(bdDatas.bd, bdDatas.user, bdDatas.password, {
-    host: bdDatas.host,
-    dialect: 'mysql'
+  host: bdDatas.host,
+  dialect: "mysql",
 });
 
-
 module.exports = {
-    Sequelize,
-    sequelize
-}
+  Sequelize,
+  sequelize,
+};
